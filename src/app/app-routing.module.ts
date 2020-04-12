89438263307c68c30bf4e3525bc967300215a975
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-12 16:26:48 +0530

    Routing added to phonelogin component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { PhoneLoginComponent } from './phone-login/phone-login.component';
 
 
 const routes: Routes = [
   { path: "", component: HomeComponent, data: {index: 0} },
   { path: "signup", component: SignupComponent, data: {index: 3} },
   { path: "login", component: LoginComponent, data: {index: 4} },
+  { path: "phoneLogin", component: PhoneLoginComponent, data: {index: 5} },
   { path: "**", component: NotFoundComponent }
 ];
 
